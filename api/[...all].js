e96b285743d3eab806e72d25139f143b5201719d
André Luiz Mazzardo <andre.mazzardo@gmail.com>
2025-09-24 19:24:11 -0300

    fix: route /api/* to serverless + proper SPA fallback

--- api/[...all].js
+++
@@ -1,9 +0,0 @@
-// api/[...all].js
-import app from '../server/index.js';
-
-// encaminha qualquer /api/* para o Express
-export default app;
-
-export const config = {
-  api: { bodyParser: false },
-};
